f49a72469b5d75619e1e8a67b7010fb2c54e6cf5
jeremy_secundaria <jeremy.secundaria@gmail.com>
2026-02-10 14:14:16 +0100

    commit del path angular frontend - Jeremy

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { HomeComponent } from './pages/home/home.component';
+import { FeaturesComponent } from './pages/features/features.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'features', component: FeaturesComponent}
+];
