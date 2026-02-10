6cde40b64a5abfb645e8687290ce34b57de46eab
Solaymane Roukdi <sroukdiamhaj@gmail.com>
2026-02-10 14:31:43 +0100

    navbar, font family

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { FeaturesComponent } from './pages/features/features.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'inicio', component: HomeComponent},
     {path: 'features', component: FeaturesComponent}
 ];
