377c456640248a38d1ad53dccdcca5496bfc09e2
Solaymane <sroukdiamhaj@gmail.com>
2026-02-16 12:22:45 +0100

    navbar y footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/layout/navbar/navbar.component';
+import { FooterComponent } from './pages/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
