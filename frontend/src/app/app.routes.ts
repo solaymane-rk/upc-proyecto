64278232ca47cec8c57074756e0cc1f7afac0f1d
duustixx <justinklk866@gmail.com>
2026-02-17 14:53:46 +0100

    Página desarrolladores web

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import path from 'node:path';
 import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
+import { DevelopersComponent } from './pages/developers/developers.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'inicio', component: HomeComponent},
-    {path: 'proyecto', component: FeaturesComponent}
+    {path: 'proyecto', component: FeaturesComponent},
+    {path: 'desarrolladores-web', component: DevelopersComponent}
 ];
--- frontend/src/app/pages/developers/developers.component.ts
+++ frontend/src/app/pages/developers/developers.component.ts
@@ -7,5 +7,79 @@ import { Component } from '@angular/core';
   styleUrl: './developers.component.css'
 })
 export class DevelopersComponent {
+  developers = [
+    {
+      id: 1,
+      nombre: 'Jose Miguel Risco Muñoz',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur.',
+      github: 'https://github.com/jose050204',
+      foto: ''
+    },
+    {
+      id: 2,
+      nombre: 'Jeremy Intriago Pachay',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur.',
+      linkedin: 'https://www.linkedin.com/in/jeremy-intriago-6735202b3/',
+      github: 'https://github.com/injerr',
+      foto: ''
+    },
+    {
+      id: 3,
+      nombre: 'Justin Monteiro Delicado',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur.',
+      linkedin: 'https://www.linkedin.com/in/justin-monteiro-delicado-109b1b262/',
+      github: 'https://github.com/duustixx',
+      foto: ''
+    },
+    {
+      id: 4,
+      nombre: 'Marc Gilavert Orea',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur.',
+      linkedin: 'https://www.linkedin.com/in/marc-gilavert-orea-b1851a2b3/',
+      github: 'https://github.com/duustixx',
+      foto: ''
+    },
+    {
+      id: 5,
+      nombre: 'Solaymane Roukdi Amhaj',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur.',
+      linkedin: 'https://www.linkedin.com/in/solaymane/',
+      github: 'https://github.com/solaymane-rk',
+      foto: ''
+    },
+  ];
+
+  habilidades = [
+    'Lorem ipsum dolor sit amet consectetur.',
+    'Lorem ipsum dolor sit amet consectetur.',
+    'Lorem ipsum dolor sit amet consectetur.',
+    'Lorem ipsum dolor sit amet consectetur.'
+  ];
+
+  indiceActual = 0;
+
+  siguiente() {
+    if (this.indiceActual < this.developers.length -3) {
+      this.indiceActual++;
+    } else {
+      this.indiceActual = 0;
+    }
+  }
+
+  anterior() {
+    if(this.indiceActual > 0) {
+      this.indiceActual--;
+    } else {
+      this.indiceActual = this.developers.length - 3;
+    }
+  }
+
+  getDevsVisibles() {
+    return this.developers.slice(this.indiceActual, this.indiceActual + 3);
+  }
+
+  getDots() {
+    return Array(this.developers.length - 2).fill(0);
+  }
 
 }
