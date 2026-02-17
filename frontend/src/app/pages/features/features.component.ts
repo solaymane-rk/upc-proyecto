e2232d4bb60d55641254acb5e9b3239dd98d153e
jeremy_secundaria <jeremy.secundaria@gmail.com>
2026-02-17 13:37:20 +0100

    Estructura features acabada por revisar contenidos/img

--- frontend/src/app/pages/features/features.component.ts
+++ frontend/src/app/pages/features/features.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../layout/navbar/navbar.component';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-features',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
@@ -14,5 +15,9 @@ export class FeaturesComponent {
 
     this.open = this.open == element.id ? "" : element.id;
     console.log(this.open)
+  }
+
+  isOpen(id:string){
+    return id == this.open;
   }
 }
