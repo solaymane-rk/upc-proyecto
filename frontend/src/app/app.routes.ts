1840244a009e49b5a4a583a4d9c62ae1e8346ff6
jeremy_secundaria <jeremy.secundaria@gmail.com>
2026-02-18 10:48:28 +0100

    Ajustes del responsive del resumen (home) + agregada la ruta del contacto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
-import path from 'node:path';
+
 import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { DevelopersComponent } from './pages/developers/developers.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'inicio', component: HomeComponent},
     {path: 'proyecto', component: FeaturesComponent},
-    {path: 'desarrolladores-web', component: DevelopersComponent}
+    {path: 'desarrolladores-web', component: DevelopersComponent},
+    {path: 'contacto', component: ContactComponent}
 ];
