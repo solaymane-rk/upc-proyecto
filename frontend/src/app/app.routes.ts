660d2fea5f2247e62d26d131d7c71dab28674434
Solaymane Roukdi <sroukdiamhaj@gmail.com>
2026-02-18 10:52:35 +0100

    font awsome y rutas contacto y login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import path from 'node:path';
 import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { DevelopersComponent } from './pages/developers/developers.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'inicio', component: HomeComponent},
     {path: 'proyecto', component: FeaturesComponent},
-    {path: 'desarrolladores-web', component: DevelopersComponent}
+    {path: 'desarrolladores-web', component: DevelopersComponent},
+    {path: 'contacto', component: ContactComponent},
+    {path: 'login', component: LoginComponent}
 ];
