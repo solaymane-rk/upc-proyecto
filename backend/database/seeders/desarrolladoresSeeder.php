e40901104666bfbfa9b6bcc5f1a09c7830bb8263
duustixx <justinklk866@gmail.com>
2026-03-12 01:31:37 +0100

    Descripciones desarrolladores web

--- backend/database/seeders/desarrolladoresSeeder.php
+++ backend/database/seeders/desarrolladoresSeeder.php
@@ -17,7 +17,7 @@ class desarrolladoresSeeder extends Seeder
             [
                 'id' => 1,
                 'nombre' => 'Jose Miguel Risco Muñoz',
-                'descripcion' => 'Lorem ipsum dolor sit amet consectetur.',
+                'descripcion' => 'Estudiante de Desarrollo de Aplicaciones Web con interés por construir proyectos funcionales y bien estructurados. Siempre en busca de nuevos conocimientos que amplíen su visión del desarrollo.',
                 'linkedin' => 'https://www.linkedin.com/in/jose-miguel-risco-mu%C3%B1oz-01b108258/',
                 'github' => 'https://github.com/jose050204',
                 'foto' => 'images/jose.jpg',
@@ -25,7 +25,7 @@ class desarrolladoresSeeder extends Seeder
             [
                 'id' => 2,
                 'nombre' => 'Jeremy Intriago Pachay',
-                'descripcion' => 'Lorem ipsum dolor sit amet consectetur.',
+                'descripcion' => 'Estudiante de DAW con curiosidad por todas las capas del desarrollo web. Le motiva entender cómo encajan el frontend y el backend para crear aplicaciones completas.',
                 'linkedin' => 'https://www.linkedin.com/in/jeremy-intriago-6735202b3/',
                 'github' => 'https://github.com/injerr',
                 'foto' => 'images/jeremy.jpg',
@@ -33,7 +33,8 @@ class desarrolladoresSeeder extends Seeder
             [
                 'id' => 3,
                 'nombre' => 'Justin Monteiro Delicado',
-                'descripcion' => 'Lorem ipsum dolor sit amet consectetur.',
+                'descripcion' => '
+Estudiante de Desarrollo de Aplicaciones Web con aspiraciones de convertirse en desarrollador full-stack. Disfruta aprendiendo tanto del lado del cliente como del servidor.',
                 'linkedin' => 'https://www.linkedin.com/in/justin-monteiro-delicado-109b1b262/',
                 'github' => 'https://github.com/duustixx',
                 'foto' => 'images/justin.jpg',
@@ -41,7 +42,7 @@ class desarrolladoresSeeder extends Seeder
             [
                 'id' => 4,
                 'nombre' => 'Marc Gilavert Orea',
-                'descripcion' => 'Lorem ipsum dolor sit amet consectetur.',
+                'descripcion' => 'Estudiante de DAW con ganas de crecer profesionalmente en el sector tecnológico. Le apasiona el proceso de transformar ideas en aplicaciones web reales y funcionales.',
                 'linkedin' => 'https://www.linkedin.com/in/marc-gilavert-orea-b1851a2b3/',
                 'github' => 'https://github.com/duustixx',
                 'foto' => 'images/marc.jpg',
@@ -49,7 +50,7 @@ class desarrolladoresSeeder extends Seeder
             [
                 'id' => 5,
                 'nombre' => 'Solaymane Roukdi Amhaj',
-                'descripcion' => 'Lorem ipsum dolor sit amet consectetur.',
+                'descripcion' => 'Estudiante de DAW con actitud proactiva y capacidad de adaptación. Comprometido con su formación y con aportar lo mejor de sí mismo en cada proyecto del equipo.',
                 'linkedin' => 'https://www.linkedin.com/in/solaymane/',
                 'github' => 'https://github.com/solaymane-rk',
                 'foto' => 'images/solaymane.jpg',
